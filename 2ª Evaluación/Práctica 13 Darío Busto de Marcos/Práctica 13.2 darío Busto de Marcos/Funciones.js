d929b49144d0438da58d5104ad070be862f2f99c
Dabusss <daribustodemarcos@gmail.com>
2026-01-23 00:02:00 +0100

    "Terminada práctica 13.2"

---
+++ 2ª Evaluación/Práctica 13 Darío Busto de Marcos/Práctica 13.2 darío Busto de Marcos/Funciones.js
@@ -0,0 +1,8 @@
+function aclarar(id_objeto) {
+    document.getElementById(id_objeto).style.opacity = "100%";
+    document.getElementById(id_objeto).style.zoom = "1.5"
+}
+function opacar(id_objeto) {
+    document.getElementById(id_objeto).style.opacity = "50%";
+    document.getElementById(id_objeto).style.zoom = "inherit"
+}
